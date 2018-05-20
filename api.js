5d035aed5b23f872d4c63466fc8fee055ca79c94
simonaslavova <monaslavova@gmail.com>
2018-05-20 14:37:14 +0100

    Api

--- api.js
+++ api.js
@@ -15,10 +15,13 @@ var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 //Pasport
+router.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+router.use(bodyParser.json());
+
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
 var MySQLStore = require('express-mysql-session')(session);
-var passport = require('passport');
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
@@ -30,12 +33,39 @@ router.use(function(req, res, next){
   next();
 });
 
-passport.use(new LocalStrategy(function(username, password, done){
+
+/*passport.use(new LocalStrategy(
+	function(username, password, done){
 	console.log(username);
 	console.log(password);
+
+    db.query('SELECT id_user, password FROM user WHERE username = ?',[username], (err,result)=>{
+      if(err)throw err;
+
+          console.log(result[0]);
+
+      //if nothing is returned
+      if(result.length===0){
+        console.log("Empty");
+        done(null,false);
+      }
+
+      const hash = result[0].password.toString();
+      var res = bcrypt.compareSync(password, hash);
+
+            if(res===true){
+
+              return done(null, {user_id:result[0].id});
+
+            }else{
+
+              done(null,false);
+            }
+
+    });
 	return done(null, 'lala');
     }
-));
+));*/
 
 const options = {
   host: "localhost",
@@ -53,7 +83,9 @@ router.use(session({
 	store: sessionStore,
 	saveUnitialized: false ,
 	//cookie:{secure:true}
-}))
+})
+);
+
 
 var con = mysql.createConnection({
 	host: "localhost",
@@ -72,43 +104,38 @@ con.connect(function(err) {
 /*Local - for local database strategy
 router.post('/start', passport.authenticate('local', {
 
-successRedirect: 'users/login',
-failureRedirect: 'users/start'
+successRedirect: '/users/profile',
+failureRedirect: '/users/login'
 
 }));
 */
 
-router.get('/start', (req,res)=>{
-//  deserializeUser ... if so - creates a session and returns a session key
-  console.log(req.user);
-  console.log(req.isAuthenticated());
-  res.render('start');
-});
-
-router.get("/profile", function(req,res){
-	//res.render('profile');
+/*router.get("/start", function(req,res){
 	con.query("SELECT * FROM `Users`", function (err, result, fields) {
 		if (err) throw err;
 		console.log(result);
-	    res.render('profile',{result: JSON.stringify(result)});
+	    res.render('start',{users: result});
 	});
 });
+*/
 
-router.get("/start", function(req,res){
-	con.query("SELECT * FROM `Users`", function (err, result, fields) {
-		if (err) throw err;
-		console.log(result);
-	    res.render('start',{result: JSON.stringify(result)});
-	});
+router.get("/start", (req,res)=>{
+	res.render('start');
+  //  deserializeUser ... if so - creates a session and returns a session key
+  //console.log(req.user);
+  //console.log(req.isAuthenticated());
+  //res.render('start');
+  //con.query("SELECT * FROM `Users`", function (err, result, fields) {
+		//if (err) throw err;
+		//console.log(result);
+	    //res.render('start',{users: result});
+	//});
 });
 
-router.post('/start',urlencodedParser, function(req, res) {
+router.post("/start", function(req, res) {
     var pass = req.body.pass;
 
 	if(req.body.pass==req.body.passC && req.body.name!="" && req.body.email!=""){
-		//pass=req.body.pass;
-		//console.log("second");
-		
 
 		bcrypt.hash(pass, saltRounds, function(err, hash){
 
@@ -123,69 +150,60 @@ router.post('/start',urlencodedParser, function(req, res) {
 				con.query(sql,(err,result)=>{
                   if(err)throw err;
 
-                  var id_user = result[0];
+                  var id_user = result[0].id_user;
                   console.log(result[0]);
 
                   //LOGIN USER-create a session
-                     req.login(id_user,function(err){
+                    /* req.login(id_user,function(err){
 
-                     //res.redirect('/users/login');  
+                     res.redirect('/users/profile');  
 
-                     });
+                     })*/;
 			     });
 		    });
-		res.redirect("/users/login");
 	    });
+	    res.redirect('/users/login');
 	}
 	else
-		res.redirect("/users/start");
+		res.redirect('/users/start');
 	
 });
 
-router.get('/submitTag',function(req,res){
-	con.query("SELECT * FROM `Tags`", function (err, result, fields) {
-		if (err) throw err;
-		console.log(result);
-		res.render('submitTag', {result: JSON.stringify(result)});
-	});
-});
-
-router.post('/submitTag', function(req, res) {
-	var q = "INSERT INTO `Tags`(`tag_name`, `category`) VALUES (?,?)";
-	var values = [req.body.Tname, req.body.Tcategory];
-	con.query(q, values, function(err, result) {
-		if(err) throw err;
-		console.log("tag submit");
-	});
-	res.redirect("/users/submitTag");
-});
-
-router.get("/main", function(req,res){
-	res.render('main');
+router.get("/profile", authenticationMiddleware(), function(req,res){
+	console.log(req.user);
+	res.render('profile');
+	/*con.query("SELECT * FROM `Taglist`", function (err, rows, fields) {
+	if (err) throw err;
+	res.render('profile',{tags: rows});
+	console.log(rows.tag_name);
+	});*/
 });
 
 router.get("/login", function(req,res){
 	res.render('login');
 });
 
-router.post("/login"
-	/*, passport.authenticate('local',{
-	successRedirect: '/profile',
-	failureRedirect: '/login'
-    })*/
-    , urlencodedParser, function(req,res){
+router.post("/login", function(req,res){
 	var username = req.body.username;
 	var password = req.body.pass;
-	let vals=[req.body.username];
-	con.query("SELECT * FROM `Users` WHERE `username`=?", vals, function (err, result, fields) {
+
+	con.query("SELECT * FROM `Users` WHERE `username`=?", username, function (err, result, fields) {
 		if(result.length>0){
-			if (bcrypt.compareSync(password, result[0].password))
-				res.redirect("/users/profile");
+			console.log("1");
+			if (bcrypt.compare(password, result[0].password)){
+				console.log("2");
+				var id_user = result[0].id_user;
+					req.login(id_user, function(err){
+						console.log(req.user);
+						//res.render('profile',{user: result});
+						res.redirect('/users/profile');
+					});
 			else
-				res.redirect("/users/login");
+			console.log("5");
+			res.redirect('/users/login');
+			}
+			
 		}
-		else
-			res.redirect("/users/login");
 	});
 });
 
@@ -253,6 +271,35 @@ router.get("/logout", function(req,res){
 });
 //>>>>>>> 0ed60bdb3005505bfd42b1a406d868b31eb1ffd6
 */
+
+router.get("/main", function(req,res){
+	con.query("SELECT * FROM `Tags`", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		//console.log(req.user);
+		res.render('main', {tags: result});
+	});
+});
+
+router.get("/submitTag",function(req,res){
+	con.query("SELECT * FROM `Tags`", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		//console.log(req.user);
+		res.render('submitTag', {tags: result});
+	});
+
+});
+
+router.post("/submitTag", function(req, res) {
+	var q = "INSERT INTO `Tags`(`tag_name`, `category`) VALUES (?,?)";
+	var values = [req.body.Tname, req.body.Tcategory];
+	con.query(q, values, function(err, result) {
+		if(err) throw err;
+		console.log("tag submit");
+	});
+	res.redirect("/users/submitTag");
+});
 
 //SHOULD UPLOAD TO TAGLIST CURRENT LOGGED IN USER AND THE INPUT OF WHATVER TAG ID WE PUT IN		
 router.get('/tagToUser',function(req,res){
@@ -327,15 +374,6 @@ router.post('/InviteToChat', function(req, res, next) {
 	res.redirect("/users/InviteToChat");
 });
 
-function displayTags (taglist)
-{
-	for(var z=0; z<taglist.length; z++)
-	{
-		console.log(taglist[z].tag_name);
-	}
-	console.log("function working");
-}
-
 router.get('/findRandomChat',(req,res)=>{
 	let sql = 'SELECT * FROM Users';
 	db.query(sql,(err,result)=>{
@@ -359,7 +397,7 @@ passport.deserializeUser(function(id_user, done) {
 
 function authenticationMiddleware() {  
   return (req, res, next) => {
-    console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+    //console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
 
       if (req.isAuthenticated()) return next();
       res.redirect('/users/login')
@@ -405,3 +443,4 @@ router.post('/imginsert',multer({
         //console.log(query.sql);
         //console.log(req.file);
     });
+
